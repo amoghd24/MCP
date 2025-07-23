e5a7e5ae17bbd45726beeec78b321593b7136164
Amogh Dikshit <amoghdikshit24@gmail.com>
2025-07-23 04:31:19 -0700

    minor additio in amp/init

--- src/tools/amplitude/__init__.py
+++ src/tools/amplitude/__init__.py
@@ -8,9 +8,19 @@ from .segmentation import (
     get_amplitude_event_segmentation_simple
 )
 from .funnel import get_amplitude_funnel
+from .get_events import (
+    get_amplitude_events_list,
+    get_amplitude_event_details
+)
+from .retention import get_amplitude_retention
+from .users import get_amplitude_users
 
 __all__ = [
     "get_amplitude_event_segmentation",
     "get_amplitude_event_segmentation_simple",
-    "get_amplitude_funnel"
+    "get_amplitude_funnel",
+    "get_amplitude_events_list",
+    "get_amplitude_event_details",
+    "get_amplitude_retention",
+    "get_amplitude_users"
 ]
